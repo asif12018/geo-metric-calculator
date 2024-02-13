447c6d656d7554923905f45a4984ff9494b084f1
asif12018 <asif.request@gmail.com>
2024-02-13 13:00:56 +0600

    js function added for triangle and rectangle

--- js/app.js
+++ js/app.js
@@ -1,12 +1,31 @@
+
+
+//common function
+
+function getValue(a){
+    let value= parseFloat(document.getElementById(a).value);
+    return value;
+};
+
+
+
+function validationResult(first,second,result){
+   if(typeof result === 'number' && !isNaN(result)){
+      document.getElementById(first).value = '';
+      document.getElementById(second).value = '';
+      const li = document.createElement('li');
+      li.innerHTML = `Area is: ${result} cm<sup>2</sup>  <span class='bg-[#1090D8] rounded-md py-2'>convert to m<sup>2</sup></span>  `;
+      document.getElementById('result-list').appendChild(li);
+     }
+}
+
 // triangle function code start from here
 
 function calculateTriangleArea(){
-   const b = parseFloat(document.getElementById('triangle-b').value);
-   const h = parseFloat(document.getElementById('triangle-h').value);
+   const b = getValue('triangle-b');
+   const h = getValue('triangle-h');
    const result = 0.5 * b * h;
-   if(typeof result === 'number' && !isNaN(result)){
-    alert(result);
-   }
+   validationResult('triangle-b','triangle-h',result);
 }
 
 
@@ -15,12 +34,8 @@ function calculateTriangleArea(){
 // rectangle code start from here
 
 function calculateRectangleArea(){
-    const w = parseFloat(document.getElementById('rectangle-w').value);
-    const i = parseFloat(document.getElementById('rectangle-i').value);
+    const w = getValue('rectangle-w');
+    const i = getValue('rectangle-w');
     const result = w * i;
-    if(typeof result === 'number' && !isNaN(result)){
-     alert(result);
-     document.getElementById('rectangle-w').value = '';
-     document.getElementById('rectangle-i').value = '';
-    }
+   validationResult('rectangle-w','rectangle-i',result);
  }
