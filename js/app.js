ee8adc0e3548eb7add84bbf61ac69a6966f9694f
asif12018 <asif.request@gmail.com>
2024-02-13 13:53:32 +0600

    every logic added

--- js/app.js
+++ js/app.js
@@ -9,12 +9,12 @@ function getValue(a){
 
 
 
-function validationResult(first,second,result){
+function validationResult(name,first,second,result){
    if(typeof result === 'number' && !isNaN(result)){
       document.getElementById(first).value = '';
       document.getElementById(second).value = '';
       const li = document.createElement('li');
-      li.innerHTML = `Area is: ${result} cm<sup>2</sup>  <span class='bg-[#1090D8] rounded-md py-2'>convert to m<sup>2</sup></span>  `;
+      li.innerHTML = `Area of ${name} is: ${result} cm<sup>2</sup>  <span class='bg-[#1090D8] rounded-md py-2'>convert to m<sup>2</sup></span>  `;
       document.getElementById('result-list').appendChild(li);
      }
 }
@@ -25,7 +25,7 @@ function calculateTriangleArea(){
    const b = getValue('triangle-b');
    const h = getValue('triangle-h');
    const result = 0.5 * b * h;
-   validationResult('triangle-b','triangle-h',result);
+   validationResult('triangle','triangle-b','triangle-h',result);
 }
 
 
@@ -37,5 +37,42 @@ function calculateRectangleArea(){
     const w = getValue('rectangle-w');
     const i = getValue('rectangle-w');
     const result = w * i;
-   validationResult('rectangle-w','rectangle-i',result);
+   validationResult('rectangle','rectangle-w','rectangle-i',result);
  }
+
+
+ // parallelogrem code start from here
+
+ function calculateParallelogrem(){
+       const b = getValue('parallelogrem-b');
+       const h = getValue('parallelogrem-h');
+       const result = b * h;
+       validationResult('parallelogrem','parallelogrem-b','parallelogrem-h', result);
+ }
+
+ // Rhombus code start from here
+
+ function calculateRhombus(){
+   const d1 = getValue('rhombus-d1');
+   const d2 = getValue('rhombus-d2');
+   const result = 0.5 * d1 * d2;
+   validationResult('rhombus','rhombus-d1','rhombus-d2',result);
+ }
+
+ //pentagon code start from here
+
+ function calculatePentagon(){
+   const p = getValue('pentagon-p');
+   const b = getValue('pentagon-b');
+   const result = 0.5 * p * b;
+   validationResult('pentagon','pentagon-p','pentagon-b',result);
+ }
+
+
+ //ellipse code start from here
+function calculateEllipse(){
+   const a = getValue('ellipse-a');
+   const b = getValue('ellipse-b');
+   const result = 3.1416 * a * b;
+   validationResult('ellipse','ellipse-a','ellipse-b',result);
+}
